9aaf16bd54e799f266479c3c5fe76148697e43eb
sanyam <patilsanyam869@gmail.com>
2023-11-04 17:16:08 +0530

    update question no-2

---
+++ ques2.js
@@ -0,0 +1,13 @@
+for(var i; i<4; i++)
+{
+    setTimeout(function(){
+        console.log(i)
+    },1000)
+}
+for(let i; i<4; i++)
+{
+    setTimeout(function(){
+        console.log(i)
+    },2000)
+}
+console.log("HII")
