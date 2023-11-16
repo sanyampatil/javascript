5d56822f6112ae2ea8019dad8cfd918b19c376c0
sanyam <patilsanyam869@gmail.com>
2023-11-16 10:03:48 +0530

    added ques  no-9

---
+++ ques9.js
@@ -0,0 +1,12 @@
+// let msg;
+// msg={data:[24]};
+// console.log(msg)
+
+// undeclared variable ko khudhi declared karke windo scope mai add krte hai
+
+"use strict"
+let msg;
+msg={data:[24]};
+console.log(msg)
+
+
