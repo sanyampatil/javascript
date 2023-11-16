9c444d1e7f82ff7ceb55db9f14540ef1cd40b2a2
sanyam <patilsanyam869@gmail.com>
2023-11-16 23:52:08 +0530

    added ques no-12

---
+++ ques12.js
@@ -0,0 +1,12 @@
+function Human(fname,lname){
+ this.firstName=fname;
+ this.lastname=lname;
+
+}
+
+const Mrx = new Human("Mr.","X");
+const Rock =Human("The","Rock");
+console.log(Mrx);
+console.log(Rock);
+
+// utana nhi samaja uske badme dekhenge
