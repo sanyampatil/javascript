e52e0d6e916932cf1bb1318b4d1042f496b290a2
sanyam <patilsanyam869@gmail.com>
2023-12-13 22:28:36 +0530

    added ques no-21

---
+++ ques21.js
@@ -0,0 +1,12 @@
+const  obj = {
+ 1:"a",
+ 2:"b"
+};
+const set = new Set([1,2,3,4,5]);
+ console.log(obj.hasOwnProperty("1"));
+ console.log(obj.hasOwnProperty(1));
+
+ console.log(set.has("1"));
+ console.log(set.has(1));
+
+
