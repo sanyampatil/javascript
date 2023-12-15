b7ca982641320c7f1aecd04014a982c00cece8f5
sanyam <patilsanyam869@gmail.com>
2023-12-15 21:49:27 +0530

    learn find and filter function

---
+++ ques-1.js
@@ -0,0 +1,24 @@
+// let arr = [1,3,1,3,4,5,6]
+// let arrNew = arr.filter((item)=>{
+//  if(item==1)
+//  {
+
+//   return item;
+//  }
+// })
+// console.log(arrNew)
+
+// ====outout==>[ 1, 1 ]
+
+
+let arr = [1,3,1,3,4,5,6]
+let arrNew = arr.find((item)=>{
+ if(item==1)
+ {
+
+  return item;
+ }
+})
+console.log(arrNew)
+
+// ====outout==> 1
